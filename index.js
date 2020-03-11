e218e436eab4151425598a176f8fd175bbe58991
Dedy <dedyjoomla@gmail.com>
2020-03-11 09:18:29 +0700

    first version

---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * Required External Modules
+ */
+var os = require('os');
+var express = require("express");
+require('custom-env').env('')
+var expressStatusMonitor = require('express-status-monitor')
+var mongooseMorgan = require('mongoose-morgan');
+var path = require("path");
+
+/**
+ * App Variables
+ */
+var app = express;
+var port = process.env.PORT || "3000";
+var osdata = {
+    platform: os.platform(),
+    release: os.release(),
+};
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(expressStatusMonitor())
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Routes Definitions
+ */
+app.get("/", () => {
+    res.render("index", {
+        title: title,
+        osPlatform: osdata.platform,
+        osRelease: osdata.release,
+    });
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
